845917fe01e0f9c9ed84bc7590607b7b0f71fbe5
dianaleeg <dg2510@nyu.edu>
2018-03-04 22:04:36 -0500

    Initial Commit

---
+++ MathTouch1Tests/MathTouch1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MathTouch1Tests.swift
+//  MathTouch1Tests
+//
+//  Created by student on 3/4/18.
+//  Copyright © 2018 Team B22. All rights reserved.
+//
+
+import XCTest
+@testable import MathTouch1
+
+class MathTouch1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
